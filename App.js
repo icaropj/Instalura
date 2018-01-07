a4db592e4104369fb0e08da390d2d7d2954a9f4d
Ícaro Pinho Esmeraldo <icaropinhoe@gmail.com>
2018-01-07 05:18:06 -0200

    added photos list in memory and not using separated components

--- App.js
+++ App.js
@@ -18,11 +18,21 @@ const width = Dimensions.get('screen').width;
 
 export default class App extends Component<{}> {
   render() {
+    const fotos = [
+      {id:1, usuario:'rafael'}, 
+      {id:2, usuario:'alberto'}, 
+      {id:3, usuario:'vitor'}
+    ];
+
     return (
       <View>
-        <Text>icaro</Text>
-        <Image source={require('./resources/img/s2-checked.png')}
-          style={{width:width, height:width}}/>
+        {fotos.map(foto => 
+          <View key={foto.id}>
+            <Text>{foto.usuario}</Text>
+            <Image source={require('./resources/img/s2-checked.png')}
+              style={{width:width, height:width}}/>
+          </View>
+        )}
       </View>
     );
   }
